62c9587885924a68af21d6c13166c2af2e1fd99e
Pritom Karmokar <pritomkarmokar99@gmail.com>
2025-05-18 00:57:14 +0600

    feat: crud functionalities added without unit tests

--- backend/patients/models.py
+++ backend/patients/models.py
@@ -1,11 +1,20 @@
+import logging
+
 from django.db import models
+from django.utils import timezone
 from django_ulid.models import ULIDField, default
+
+logger = logging.getLogger(__name__)
 
 class Patient(models.Model):
     id = ULIDField(primary_key=True, default=default, editable=False)
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
     blood_group = models.CharField(max_length=20)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.first_name
@@ -13,3 +22,31 @@ class Patient(models.Model):
     class Meta:
         verbose_name = "Patient"
         db_table = "patient"
+
+    def update_model(
+            self, 
+            first_name: str,
+            last_name: str,
+            blood_group: str
+    ) -> bool:
+        try:
+            if first_name:
+                self.first_name = first_name
+            
+            if last_name:
+                self.last_name = last_name
+            
+            if blood_group:
+                self.blood_group = blood_group
+            
+            self.updated_at = timezone.now()
+            self.save()
+            return True
+        except Exception as e:
+            logger.error(
+                {
+                    "message": "Error occurred while updating patient object",
+                    "errors": repr(e)
+                }
+            )
+            return False
--- backend/patients/views.py
+++ backend/patients/views.py
@@ -1,3 +1,99 @@
-from django.shortcuts import render
+from django.utils import timezone
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from .models import Patient
+from .serializers import PatientSerializer
+
+class PatientsListCreateAPIView(APIView):
+    serializer_class = PatientSerializer
+
+    def get(self, request: Request) -> Response:
+        patients = Patient.objects.all()
+        
+        if len(patients) <= 0:
+            response = {
+                "message": "Currently, no patients info is available"
+            }
+            return Response(data=response, status=status.HTTP_204_NO_CONTENT)
+        
+        serializer = self.serializer_class(instance=patients, many=True)
+        
+        response = {
+            "message": "Currently available patients",
+            "data": serializer.data
+        }
+        return Response(data=response, status=status.HTTP_200_OK)
+
+    def post(self, request: Request) -> Response:
+        data = request.data
+        serializer = self.serializer_class(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            response = {
+                "message": "New patient info added successfully",
+                "data": serializer.data
+            }
+            return Response(data=response, status=status.HTTP_201_CREATED)
+        
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class PatientsRetrieveUpdateDeleteAPIView(APIView):
+    serializer_class = PatientSerializer
+
+    def get(self, request: Request, patient_id: int) -> Response:
+        patient = Patient.objects.get(id=patient_id)
+
+        if patient:
+            serializer = self.serializer_class(data=patient)
+            response = {
+                "message": f"Patient with the Id {patient_id} retrieved successfully",
+                "data": serializer.data
+            }
+            return Response(data=response, status=status.HTTP_200_OK)
+        
+        response = {
+            "message": f"Patient with the following Id {patient_id} does not exist"
+        }
+        return Response(data=response, status=status.HTTP_404_NOT_FOUND)
+    
+    def patch(self, request: Request, patient_id: int) -> Response:
+        first_name = request.data.get("first_name", None)
+        last_name = request.data.get("last_name", None)
+        blood_group = request.data.get("blood_group", None)
+
+        patient = Patient.objects.get(id=patient_id)
+
+        if not patient:
+            respone = {
+                "message": f"Patient with the following Id {patient_id} doesn't exist"
+            }
+            return Response(data=respone, status=status.HTTP_404_NOT_FOUND)
+        
+        if not first_name and not last_name and not blood_group:
+            respone = {
+                "message": "first_name, last_name or blood_group is required. All values cannot be empty"
+            }
+            return Response(data=respone, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            _ = patient.update_model(first_name, last_name, blood_group)
+            respone = {
+                "messsage": f"Patient info with Id {patient_id} updated successfully"
+            }
+            return Response(data=respone, status=status.HTTP_200_OK)
+        
+    def delete(self, request: Request, patient_id: int) -> Response:
+        patient = Patient.objects.get(id=patient_id)
+        patient.is_active = False
+        patient.deleted_at = timezone.now()
+        patient.save()
+
+        respone = {
+            "message" : "Patient Info deleted successfully"
+        }
+        return Response(data=respone, status=status.HTTP_204_NO_CONTENT)
