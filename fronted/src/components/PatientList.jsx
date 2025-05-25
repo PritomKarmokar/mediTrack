a0ac4ea5939001137752f431b53da7e07207cec4
Pritom Karmokar <pritomkarmokar99@gmail.com>
2025-05-25 22:01:26 +0600

    feat: adding delete option fronted part

--- fronted/src/components/PatientList.jsx
+++ fronted/src/components/PatientList.jsx
@@ -1,27 +1,39 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import {AddPatient} from "./AddPatient.jsx";
+import { AddPatient } from "./AddPatient.jsx";
 
 export function PatientList() {
     const [patients, setPatients] = useState([])
     const [showAddPatient, setShowAddPatient] = useState(false)
 
-    const fetchPatients = () =>{
+    useEffect(() => {
+        fetchPatients()
+    }, [])
+
+    const fetchPatients = () => {
         axios.get("http://localhost:8000/patients/")
             .then(response => {
                 console.log(response.data)
                 let patientsData = response.data
                 setPatients(patientsData.data)
             })
-            .catch(error =>{
+            .catch(error => {
                 console.log(error)
             })
     }
-    useEffect(() => {
-        fetchPatients()
-    },[])
 
-    const handleCancelBtn = () =>{
+    const handleDeleteBtn = (id) => {
+        axios.delete("http://localhost:8000/patients/" + id + "/")
+            .then(response => {
+                console.log(response.data)
+                setPatients(patients.filter(p => p.id !== id))
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
+    const handleCancelBtn = () => {
         setShowAddPatient(false)
         fetchPatients()
     }
@@ -38,24 +50,24 @@ export function PatientList() {
                     </tr>
                 </thead>
                 <tbody>
-                {patients.map(patient => (
-                    <tr key={patient.id}>
-                        <td>{patient.first_name}</td>
-                        <td>{patient.last_name}</td>
-                        <td>{patient.blood_group}</td>
-                        <td>
-                            <button className="btn btn-primary m-2" onClick={() =>{}}>Update</button>
-                            <button className="btn btn-danger" onClick={() =>{}}>Delete</button>
-                        </td>
-                    </tr>
-                ))}
+                    {patients.map(patient => (
+                        <tr key={patient.id}>
+                            <td>{patient.first_name}</td>
+                            <td>{patient.last_name}</td>
+                            <td>{patient.blood_group}</td>
+                            <td>
+                                <button className="btn btn-primary m-2" onClick={() => { }}>Update</button>
+                                <button className="btn btn-danger" onClick={() => { handleDeleteBtn(patient.id) }}>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
             <button className="btn btn-success" onClick={() => setShowAddPatient(true)}>
                 Add New Patient
             </button>
             <br></br>
-            {showAddPatient && <AddPatient handleCancelBtn={handleCancelBtn} refreshPatients={fetchPatients}/>}
+            {showAddPatient && <AddPatient handleCancelBtn={handleCancelBtn} refreshPatients={fetchPatients} />}
         </div>
     )
 }
